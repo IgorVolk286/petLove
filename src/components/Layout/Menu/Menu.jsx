61d34caa0ab71ab17dde938f1babb75f0583f6b8
IgorVolk286 <125205247+IgorVolk286@users.noreply.github.com>
2024-03-28 23:12:35 +0200

    fix link

--- src/components/Layout/Menu/Menu.jsx
+++ src/components/Layout/Menu/Menu.jsx
@@ -29,7 +29,9 @@ export const Menu = ({ menu, toggle, color }) => {
       </ButtonClose>
       <DivButtons>
         <Buttons type="button">
-          <Link to="/news">News</Link>
+          {' '}
+          News
+          <Link to="/news"></Link>
         </Buttons>
         <Buttons type="button">Find pet</Buttons>
         <Buttons type="button">Our friends</Buttons>
