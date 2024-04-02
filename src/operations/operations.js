db4cae5d5e28d039c2914787a10f52018ef15499
IgorVolk286 <125205247+IgorVolk286@users.noreply.github.com>
2024-04-02 21:43:25 +0300

    fix

--- src/operations/operations.js
+++ src/operations/operations.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 axios.defaults.baseURL = 'https://petlove.b.goit.study/api';
 
-export const getNews = async () => {
-  try {
-    const response = await axios.get('/news');
-    const results = await response.json();
-    console.log(results);
-    return results;
-  } catch (error) {
-    console.error(error);
-  }
-};
+// export const getNews = async () => {
+//   try {
+//     const response = await axios.get('/news');
+//     const results = await response.json();
+//     console.log(results);
+//     return results;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
